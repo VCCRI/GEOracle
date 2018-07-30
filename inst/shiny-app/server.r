baeef1c1ac8cc48506473359f14996551e6567ea
Joshua WK Ho <j.ho@victorchang.edu.au>
2018-07-30 14:26:02 +1000

    Update server.r

--- inst/shiny-app/server.r
+++ inst/shiny-app/server.r
@@ -62,6 +62,39 @@ load(paste(extdata_path,"GSEClassifierData/PertModel.RData",sep="/"))
 # GEOmetaDB functions
 ################################################################################################################################################
 
+###
+# This function over-ride the getSQLiteFile function in Geometadb 
+###
+getSQLiteFile<-function (destdir = getwd(), destfile = "GEOmetadb.sqlite.gz") 
+{
+    localfile <- file.path(destdir, destfile)
+    url_geo_1 = "https://gbnci-abcc.ncifcrf.gov/geo/GEOmetadb.sqlite.gz"
+    url_geo_2 = "http://starbuck1.s3.amazonaws.com/sradb/GEOmetadb.sqlite.gz"
+    url_geo_3 = "http://watson.nci.nih.gov/~zhujack/GEOmetadb.sqlite.gz"
+    if (!inherits(try(url(url_geo_1, open = "rb"), silent = TRUE), 
+        "try-error")) {
+        url_geo = url_geo_1
+    }
+    else if (!inherits(try(url(url_geo_2, open = "rb"), silent = TRUE), 
+        "try-error")) {
+        url_geo = url_geo_2
+    }
+    else {
+        url_geo = url_geo_3
+    }
+    download.file(url_geo, destfile = localfile, mode = "wb")
+    cat("Unzipping...\n")
+    gunzip(localfile, overwrite = TRUE)
+    unzippedlocalfile <- gsub("[.]gz$", "", localfile)
+    con <- dbConnect(SQLite(), unzippedlocalfile)
+    dat <- dbGetQuery(con, "select * from metaInfo")
+    dbDisconnect(con)
+    cat("Metadata associate with downloaded file:\n")
+    print(dat)
+    return(unzippedlocalfile)
+}
+
+
 
 # This function downloads and loads the metadata
 loadMetadata <- function(default=TRUE){
